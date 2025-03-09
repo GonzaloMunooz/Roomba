f56bf364daf2c5b2de0db124fad30f14dfa65472
GonzaloMunooz <152875954+GonzaloMunooz@users.noreply.github.com>
2025-03-09 16:10:49 +0100

    Update Roomba.py

--- Roomba.py
+++ Roomba.py
@@ -1,20 +1,24 @@
 import pygame
-
-def lerp(start, end, t):
-    return start + t * (end - start)
+from utils import lerp
 
 class Roomba:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.size = 20
-        self.color = (255, 0, 0)
         self.target_x = x
         self.target_y = y
+        self.size = 20
+        self.speed = 15
 
     def update_position(self):
-        self.x = lerp(self.x, self.target_x, 0.1)
-        self.y = lerp(self.y, self.target_y, 0.1)
+        self.x = lerp(self.x, self.target_x, 0.45)
+        self.y = lerp(self.y, self.target_y, 0.45)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.size)
+        pygame.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), self.size)
+
+    
+
+    def move(self, dx, dy):
+        self.target_x += dx * self.speed
+        self.target_y += dy * self.speed
