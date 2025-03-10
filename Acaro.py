aa1ee140ca6d7596f3f9b509dd40e951692dbb7d
GonzaloMunooz <152875954+GonzaloMunooz@users.noreply.github.com>
2025-03-10 23:09:53 +0100

    Update Acaro.py

--- Acaro.py
+++ Acaro.py
@@ -1,24 +1,21 @@
 import pygame
-from utils import lerp
+import random
 
-class Roomba:
+class Acaro:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.target_x = x
-        self.target_y = y
         self.size = 20
-        self.speed = 15
-
-    def update_position(self):
-        self.x = lerp(self.x, self.target_x, 0.45)
-        self.y = lerp(self.y, self.target_y, 0.45)
+        self.color = (0, 0, 255)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (0, 0, 0), (int(self.x), int(self.y)), self.size)
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
 
-    
+    def is_touched(self, roomba_x, roomba_y, roomba_size):
+        distance = ((self.x - roomba_x) ** 2 + (self.y - roomba_y) ** 2) ** 0.5
+        return distance < self.size + roomba_size
 
-    def move(self, dx, dy):
-        self.target_x += dx * self.speed
-        self.target_y += dy * self.speed
+    def update(self, roomba_x, roomba_y, roomba_size):
+        if self.is_touched(roomba_x, roomba_y, roomba_size):
+            return True
+        return False
